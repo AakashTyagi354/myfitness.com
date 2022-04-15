714d09daa9331032c09cab5c561a3973a1fb0345
Aakash Tyagi <78840211+AakashTyagi354@users.noreply.github.com>
2022-04-15 12:04:50 +0530

    components and pages folder added

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 
 import './App.scss';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-    hello world
+   <Navbar/>
     </div>
   );
 }
