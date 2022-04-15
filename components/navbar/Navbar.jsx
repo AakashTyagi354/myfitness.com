98ae7314ce7114169fa27591457730328ffdd33b
Aakash Tyagi <78840211+AakashTyagi354@users.noreply.github.com>
2022-04-15 12:48:56 +0530

    navbar created

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import "./navbar.scss"
 
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='navbar'>
+            <div className="left">
+                <p>myfitness.com</p>
+            </div>
+            <div className="center">
+                <ul className="links">
+                    
+                    <li>Shop</li>
+
+                    <li>Tracker</li>
+                    <li>Workouts</li>
+
+                    <li>Community</li>
+
+                    <li>Blogs</li>
+                </ul>
+            </div>
+            <div className="right">
+                <div className="btn">
+                    <div className="login">Log In</div>
+                    
+                    <div className="signup">Sign Up</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
